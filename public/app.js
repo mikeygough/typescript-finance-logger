1285310ef1f4072a9fd4c9c86a4fe11663ee5b4d
mikey <mikey@hey.com>
2023-11-17 17:43:13 -0600

    Interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,33 +1,22 @@
-// interfaces
-// interfaces allow us to force a certain structure on a class or object
-// must follow the structure of the interface
-const me = {
-    name: "mikey",
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log("I spent", amount);
-        return amount;
-    },
-};
-// function that takes in an object of interface IsPerson
-const greetPerson = (person) => {
-    console.log("hello", person.name);
-};
-greetPerson(me);
-console.log(me);
 import { Invoice } from "./classes/invoice.js";
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-// only objects created with the invoice class can be added to the array
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from "./classes/payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// // only objects created with the invoice class can be added to the array
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 // form
 const form = document.querySelector(".new-item-form");
 // inputs
@@ -37,5 +26,12 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // prevent page refresh
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,30 @@
-// interfaces
-// interfaces allow us to force a certain structure on a class or object
-
-// we dont use isperson to create new objects
-// we just use it enforce shape
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-// must follow the structure of the interface
-const me: IsPerson = {
-  name: "mikey",
-  age: 30,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log("I spent", amount);
-    return amount;
-  },
-};
-
-// function that takes in an object of interface IsPerson
-const greetPerson = (person: IsPerson) => {
-  console.log("hello", person.name);
-}
-
-greetPerson(me);
-console.log(me);
-
 import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-// only objects created with the invoice class can be added to the array
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
 
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+// // only objects created with the invoice class can be added to the array
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 // form
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
@@ -57,5 +38,12 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault(); // prevent page refresh
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  console.log(doc);
 });
