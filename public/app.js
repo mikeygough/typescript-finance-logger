0a765e44688c581a761d7ec5f572484c062f4723
mikey <mikey@hey.com>
2023-11-18 13:03:39 -0600

    add html rendering

--- public/app.js
+++ public/app.js
@@ -1,22 +1,6 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice("mario", "work on the mario website", 250);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-// // only objects created with the invoice class can be added to the array
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
+import { ListTemplate } from "./classes/listTemplate.js";
 // form
 const form = document.querySelector(".new-item-form");
 // inputs
@@ -24,14 +8,18 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // prevent page refresh
     let doc;
-    if (type.value === 'invoice') {
+    if (type.value === "invoice") {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
+    list.render(doc, type.value, "end");
     console.log(doc);
 });
